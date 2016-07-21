3c2281b20e56cd2f75a40c8666f33e5a3ece1e3c
mmoghaddam385 <m.moghaddam385@outlook.com>
2016-07-21 20:31:07 +0200

    Working maps with location and valid radius

--- droid-app/HackAccessibility/app/src/main/java/hackupc/gatech/hackaccessibility/MapsActivity.java
+++ droid-app/HackAccessibility/app/src/main/java/hackupc/gatech/hackaccessibility/MapsActivity.java
@@ -3,14 +3,19 @@ package hackupc.gatech.hackaccessibility;
 import android.*;
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationListener;
 import android.os.Bundle;
+import android.os.Looper;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,15 +26,21 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener,
         GoogleMap.OnInfoWindowClickListener,
         GoogleMap.OnMapClickListener {
 
     private static final int CIRCLE_FILL_COLOR = 0x502493B5;
 
+    private static final int GET_LOCATION_PERMISSION_CODE = 1000;
+
     private static final double MILES_TO_METERS = 1609.34;
+    private static final double CIRCLE_RADIUS = 10 * MILES_TO_METERS;
+
     private static final float INITIAL_ZOOM_LEVEL = 10.33f;
 
+    private LatLng mCurLocLatLng = null;
+    private LocationManager mLocationManager;
     private GoogleApiClient mApiClient;
     private GoogleMap mMap;
 
@@ -47,6 +58,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     .addApi(LocationServices.API)
                     .build(); // TODO: add listeners and stuff
         }
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
     }
 
 
@@ -64,35 +79,16 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION});
+
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    GET_LOCATION_PERMISSION_CODE);
             return;
         }
-        Location curLocation = LocationServices.FusedLocationApi.getLastLocation(mApiClient);
+        Log.d("onMapReady", "Map ready, request location");
 
-        // Add a marker in Sydney and move the camera
-        LatLng bcn = new LatLng(41.388976799774106,2.164280153810978);
-        mMap.addMarker(new MarkerOptions().position(bcn).title("Marker in BCN"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(bcn));
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(INITIAL_ZOOM_LEVEL));
-
-        CircleOptions clickableRegion = new CircleOptions();
-        clickableRegion.center(bcn);
-        clickableRegion.radius(10 * MILES_TO_METERS);
-        clickableRegion.fillColor(CIRCLE_FILL_COLOR);
-        clickableRegion.strokeWidth(0);
+        mLocationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, this, null);
 
 
-        mMap.addCircle(clickableRegion);
-
-        mMap.setOnInfoWindowClickListener(this);
-        mMap.setOnMapClickListener(this);
 
     }
 
@@ -103,6 +99,75 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapClick(LatLng latLng) {
-        Log.d("Map Clicked", "Map Clicked! loc: " + latLng.toString());
+        float[] distance = new float[1];
+        Location.distanceBetween(mCurLocLatLng.latitude, mCurLocLatLng.longitude,
+                                 latLng.latitude, latLng.longitude, distance);
+
+        if (distance[0] <= CIRCLE_RADIUS) {
+            //add new thingy
+        }
+        else {
+            Toast.makeText(this, "You can only make new posts within 10 miles of your current location", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        int n = 0;
+        for (String permission : permissions) {
+            if (permission.equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                if (grantResults[n] == PackageManager.PERMISSION_GRANTED) {
+                    if (requestCode == GET_LOCATION_PERMISSION_CODE) {
+                        onMapReady(mMap);
+                    }
+                }
+                else {
+                    Toast.makeText(this, "You must grant permission for the app to work...", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            n++;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("onLocationChanged", "location changed!");
+
+        mCurLocLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        LatLng bcn = new LatLng(41.388976799774106,2.164280153810978);
+
+        mMap.addMarker(new MarkerOptions().position(bcn).title("Marker in BCN"));
+        mMap.addMarker(new MarkerOptions().position(mCurLocLatLng).title("Current Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mCurLocLatLng));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(INITIAL_ZOOM_LEVEL));
+
+        CircleOptions clickableRegion = new CircleOptions();
+        clickableRegion.center(mCurLocLatLng);
+        clickableRegion.radius(CIRCLE_RADIUS);
+        clickableRegion.fillColor(CIRCLE_FILL_COLOR);
+        clickableRegion.strokeWidth(0);
+
+
+        mMap.addCircle(clickableRegion);
+
+        mMap.setOnInfoWindowClickListener(this);
+        mMap.setOnMapClickListener(this);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        Log.d("onStatuschanged", "Status Changed!");
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        Log.d("onProviderEnabled", "Provider Enabled!");
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Log.d("onProviderDosanled", "Provider Disabled");
     }
 }
---
+++ droid-app/HackAccessibility/app/src/main/java/hackupc/gatech/hackaccessibility/model/Post.java
@@ -0,0 +1,10 @@
+package hackupc.gatech.hackaccessibility.model;
+
+/**
+ * Created by michael on 7/21/16.
+ */
+public class Post {
+    
+
+
+}
